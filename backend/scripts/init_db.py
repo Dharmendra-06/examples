9cecc3991f7f587f1e4b1106478e75fd2ddc7936
justdk06 <justdk06@gmail.com>
2025-11-08 03:52:45 +0530

    Add user creation logic in init_db script for initial roles

--- backend/scripts/init_db.py
+++ backend/scripts/init_db.py
@@ -1,6 +1,10 @@
 from app import app
 from models import db
 from flask_security.datastore import SQLAlchemyUserDatastore
+from flask_security.utils import hash_password
+
+# run like this from backend folder
+# python3 -m scripts.init_db
 
 with app.app_context():
   db.drop_all() # delete all data
@@ -11,6 +15,29 @@ with app.app_context():
   manager_role = datastore.find_or_create_role('manager', description = 'handles and manges store')
   customer_role = datastore.find_or_create_role('customer', description = 'buys items from store')
   
+  if not datastore.find_user(email = 'admin@study'):
+    datastore.create_user(
+      email = 'admin@study',
+      password = hash_password('Admin@123'),
+      name = 'admin_01'
+    )
+
+  if not datastore.find_user(email = 'manager@study'):
+    datastore.create_user(
+      email = 'manager@study',
+      password = hash_password('manager@123'),
+      name = 'manager_01'
+    )
+
+  if not datastore.find_user(email = 'customer@study'):
+    datastore.create_user(
+      email = 'customer@study',
+      password = hash_password('customer@123'),
+      name = 'customer_01'
+    )
+
+
+
   try:
     db.session.commit()
     print('✅ Initial roles created successfully.')
